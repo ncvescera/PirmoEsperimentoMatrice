8bc6c050f915992207db3dd0fb3bd9da8cb5ca15
Nicolò Vescera <slayercandy@icloud.com>
2015-02-28 09:52:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,12 +12,13 @@
  * 
  */
 int main(int argc, char** argv) {
-    int nrighe=3;
-    int ncolonne=4;
+    const int nrighe=4;
+    const int ncolonne=4;
+    int scambia[ncolonne];
     int matrice[nrighe][ncolonne];
     int righe,colonne;
-    int numero=0;
-    int ris;
+   
+
     for(righe=0;righe<nrighe;righe++){
         for(colonne=0;colonne<ncolonne;colonne++){
             printf("Inserisci un numero che andrà nella riga %d e nella colonna %d: ",righe,colonne);
@@ -29,8 +30,23 @@ int main(int argc, char** argv) {
             printf("riga %d & colonne %d = %d\n",righe,colonne,matrice[righe][colonne]);
         }
     }
-    ris=matrice[2][0];
-            printf("aaaaaaa %d",ris);
+    printf("\n");
+    int i;
+    for(i=1;i<nrighe;i+=2){
+        for(colonne=0;colonne<nrighe;colonne++){
+            scambia[colonne]=matrice[i][colonne];
+            matrice[i][colonne]=matrice[i-1][colonne];
+            matrice[i-1][colonne]=scambia[colonne];
+            
+        }
+        
+    }
+    for(righe=0;righe<nrighe;righe++){
+        for(colonne=0;colonne<ncolonne;colonne++){
+            printf("riga %d & tcolonne %d = %d\n",righe,colonne,matrice[righe][colonne]);
+        }
+    }
+            
     return (EXIT_SUCCESS);
 }
 
