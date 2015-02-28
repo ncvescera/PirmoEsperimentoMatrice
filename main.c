31dd04033d5eb2a3242ce9f9278647eada99ff51
Nicolò Vescera <slayercandy@icloud.com>
2015-02-28 09:03:50 +0100

    First Update

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Studenti
+ *
+ * Created on 28 febbraio 2015, 9.02
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
